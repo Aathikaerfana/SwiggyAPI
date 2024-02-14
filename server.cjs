99237530c56b2d0de1eb8667058c3a8a6d64ec3e
Aathikaerfana <aathikamusthaba>
2024-02-14 13:54:05 +0530

    deleting data

--- server.cjs
+++ server.cjs
@@ -3,12 +3,13 @@ const cors = require('cors')
 const express = require('express')
 const mongoose = require('mongoose')
 
-const {Restaurant,Users} = require('./schema.cjs')
+const {Restaurant, Users} = require('./schema.cjs')
 
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
+// Connecting with DB
 async function connectToDb() {
     try {
         await mongoose.connect('mongodb+srv://aathikaerfana:Aathi@cluster0.hpk6eh3.mongodb.net/Swiggy?retryWrites=true&w=majority')
@@ -24,76 +25,113 @@ async function connectToDb() {
 }
 connectToDb()
 
-app.post('/add-restaurant',async function(request,response) {
-    try{                                                     
-     await Restaurant.create({                                
-         "areaName" : request.body.areaName,          
-         "avgRating" : request.body.avgRating,
-         "costForTwo" : request.body.costForTwo,
-         "cuisines" : request.body.cuisines,
-         "name" : request.body.name
-     })
-     response.status(201).json({                     
-         "status":"successfully created"  ,
-         "message" :  "restaurant entry successfull"                     
-     })
- } catch(error){
-     response.status(500).json({                     
-         "status":"failure",           
-         "error" : "server error"
-     })
- }
-})
-app.get('/get-restaurant-details', async function(request,response) {
-    try{                                                     
-     const restaurantDetails = await Restaurant.find()
-     response.status(200).json(restaurantDetails)
- } catch(error){
-     response.status(500).json({                     
-         "status":"failure",           
-         "error" : "server error"
-     })
- }
+/**
+ * /add-restaurant : post
+ * /get-restaurant-details : get
+ * /update-restaurant-detail : patch
+ * /delete-restaurant-detail : delete
+ * /create-new-user : post
+ * /validate-user : post
+ */
+
+app.post('/add-restaurant', async function(request, response) {
+    try {
+        await Restaurant.create({
+            "areaName" : request.body.areaName,
+            "avgRating" : request.body.avgRating,
+            "costForTwo" : request.body.costForTwo,
+            "cuisines" : request.body.cuisines,
+            "name" : request.body.name
+        })
+        response.status(201).json({
+            "status" : "success",
+            "message" : "restaurant entry successful"
+        })
+    } catch(error) {
+        response.status(500).json({
+            "status" : "failure",
+            "message" : "restaurant entry unsuccessful",
+            "error" : error
+        })
+    }
 })
 
-app.post('/create-new-user',async function(request,response) {
-    try{                                                      
-     await Users.create({                                
-         "userName" : request.body.userName,          
-         "contact" : request.body.contact,
-         "email" : request.body.email,
-         "password" : request.body.password
-     })
-     response.status(201).json({                     
-         "status":"user successfully created"  ,
-         "message" :  "user created"                     
-     })
- } catch(error){
-     response.status(500).json({                     
-         "status":"user not created",           
-         "error" : "server error"
-     })
- }
- })
- app.post('/validate-user',async function(request,response) {
-     try{
-        const user = await Users.findOne({
+app.get('/get-restaurant-details', async function(request, response) {
+    try {
+        const restaurantDetails = await Restaurant.find()
+        response.status(200).json(restaurantDetails)
+    } catch(error) {
+        response.status(500).json({
+            "status" : "failure",
+            "message" : "could not fetch",
+            "error" : error
+        })
+    }
+})
+
+app.delete('/delete-restaurant-detail/:id', async function(request, response) {
+    try {
+        const restaurant = await Restaurant.findById(request.params.id)
+        if(restaurant) {
+            await Restaurant.findByIdAndDelete(request.params.id)
+            response.status(200).json({
+                "status" : "success",
+                "message" : "deleted successfully"
+            })
+        } else { //restaurant : null
+            response.status(404).json({
+                "status" : "failure",
+                "message" : "entry not found"
+            })
+        }
+    } catch(error) {
+        response.status(500).json({
+            "status" : "failure",
+            "message" : "could not delete",
+            "error" : error
+        })
+    }
+}) 
+
+app.post('/create-new-user', async function(request, response) {
+    try {
+         await Users.create({
+             "userName" : request.body.userName,
              "email" : request.body.email,
-             "password" :request.body.password
+             "password" : request.body.password,
+             "contact" : request.body.contact
          })
-         if(user){
+         response.status(201).json({
+         "status" : "success",
+         "message" : "user created"
+         })
+    } catch(error) {
+         response.status(500).json({
+             "status" : "failure",
+             "message" : "internal server error"
+         })
+    }
+ })
+ 
+ app.post('/validate-user', async function(request, response) {
+     try {
+         const user = await Users.findOne({
+             "email" : request.body.email,
+             "password" : request.body.password 
+         })
+         if(user) {
              response.status(200).json({
                  "message" : "valid user"
              })
-         }else{
+         } else {
              response.status(401).json({
                  "message" : "invalid user"
              })
          }
-     }catch(error)
-     {
-         response.status(500).json({                     
-             "error" : "internal server error"
+     } catch(error) {
+         response.status(500).json({
+             "message" : "internal server error"
          })
      }
- })
+})
+ //65cafdfdcb9278a84c1f6de9
